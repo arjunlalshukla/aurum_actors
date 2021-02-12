beb15f87c3bfa463c694f2280f53bf685e251577
Arjun Shukla <arjunlalshukla@gmail.com>
2021-02-12 13:23:52 -0700

    Fully macro-ized SpecificInterface

--- interface-proc/src/lib.rs
+++ interface-proc/src/lib.rs
@@ -7,6 +7,7 @@ use syn;
 pub fn aurum_interface(item: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(item).unwrap();
     let type_id = ast.ident.to_string();
+    let type_remote = ast.attrs.iter().any(|attr| attr.path.is_ident("aurum"));
     let data_enum: syn:: DataEnum = match ast.data {
       syn::Data::Enum(x) => x,
       _ => panic!("Only enums are supported")
@@ -14,7 +15,7 @@ pub fn aurum_interface(item: TokenStream) -> TokenStream {
     let variants: Vec<&syn::Variant> = data_enum.variants.iter().collect();
     let translates = variants.iter().filter_map(|x| aurum_tagged(*x))
       .collect::<Vec<AurumVariant>>();
-    let impls = translates.iter().map(|av: &AurumVariant| {
+    let mut impls = translates.iter().map(|av: &AurumVariant| {
       let f = formatdoc! {"
         impl std::convert::From<{from}> for {id} {{
           fn from(item: {from}) -> {id} {{
@@ -28,8 +29,72 @@ pub fn aurum_interface(item: TokenStream) -> TokenStream {
       };
       f.to_string()
     }).collect::<Vec<String>>();
+    let non_locals = translates.iter().filter(|x| x.non_local)
+      .collect::<Vec<&AurumVariant>>();
+    if type_remote {
+      impls.push(format!("impl aurum::actor::HasInterface<{}> for {} {{}}\n",
+        type_id, type_id));
+    }
+    for av in &non_locals {
+      impls.push(format!("impl aurum::actor::HasInterface<{}> for {} {{}}\n",
+        av.field, type_id));
+    }
 
-    let code = impls.join("\n");
+
+    impls.push(formatdoc! {"
+      impl<Unified> aurum::actor::SpecificInterface<Unified> for {type_id} 
+       where Unified: Eq + Debug",
+       type_id = type_id
+    });
+    if type_remote {
+      impls.push(format!(" + aurum::unify::Case<{}>", type_id));
+    }
+    for av in &non_locals {
+      impls.push(format!(" + aurum::unify::Case<{}>", av.field));
+    }
+    impls.push(formatdoc! {" 
+        {{
+          fn deserialize_as(item: Unified, bytes: Vec<u8>) ->
+           Result<Self, aurum::actor::DeserializeError<Unified>> {{
+      "
+    });
+    if !type_remote && non_locals.is_empty() {
+      impls.push(String::from(
+        "    Result::Err(DeserializeError::IncompatibleInterface(item))\n"));
+    } else {
+      if type_remote {
+        impls.push(formatdoc! {"
+          // 
+              if <Unified as aurum::unify::Case<{case}>>::VARIANT == item {{
+                deserialize::<Unified, {case}, {case}>(item, bytes)
+              }} else
+          ",
+          case = type_id
+        });
+      }
+      for av in &non_locals {
+        impls.push(formatdoc! {" 
+          //
+              if <Unified as aurum::unify::Case<{interface}>>::VARIANT == item {{
+                deserialize::<Unified, {case}, {interface}>(item, bytes)
+              }} else
+          ",
+          case = type_id,
+          interface = av.field
+        });
+      }
+      impls.push(formatdoc! {"
+        //
+            {{
+              Result::Err(DeserializeError::IncompatibleInterface(item))
+            }} 
+        "
+      });
+    }
+
+    impls.push(String::from("  }\n}"));
+
+    let code = impls.join("");
     println!("Generated code for {}: \n\n{}", type_id, code);
 
     code.parse().unwrap()
@@ -38,7 +103,8 @@ pub fn aurum_interface(item: TokenStream) -> TokenStream {
 #[derive(Debug)]
 struct AurumVariant {
   variant: String,
-  field: String
+  field: String,
+  non_local: bool
 }
 
 fn aurum_tagged(variant: &syn::Variant) -> Option<AurumVariant> {
@@ -59,7 +125,7 @@ fn aurum_tagged(variant: &syn::Variant) -> Option<AurumVariant> {
     syn::Type::Path(p) => path_to_string(&p.path),
     _ => panic!("Type is not a type path")
   };
-  Some(AurumVariant { variant: id, field: type_str } )
+  Some(AurumVariant { variant: id, field: type_str, non_local: true} )
 }
 
 fn path_to_string(path: &syn::Path) -> String {
--- src/bin/typed-ref-test.rs
+++ src/bin/typed-ref-test.rs
@@ -1,5 +1,4 @@
-use aurum::{actor::{Host, Node, HasInterface}, registry::Registry};
-use aurum::actor::{SpecificInterface, DeserializeError};
+use aurum::actor::{DeserializeError, Host, Node, SpecificInterface};
 use aurum::registry::RegistryMsg;
 use aurum::unify::Case;
 use aurum::unified;
@@ -7,13 +6,14 @@ use interface_proc::AurumInterface;
 use aurum::actor::deserialize;
 use serde::{Serialize, Deserialize};
 use std::fmt::Debug;
-use std::collections::HashMap;
+//use std::collections::HashMap;
 
 #[allow(dead_code)] type Unsigned32 = u32;
 #[allow(dead_code)] type MaybeString = Option<String>;
 #[allow(dead_code)] type RegMsg = RegistryMsg<MsgTypes>;
 
 #[derive(AurumInterface, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[aurum]
 enum LoggerMsg { 
   Info(String),
   #[aurum]
@@ -21,42 +21,75 @@ enum LoggerMsg {
   #[aurum]
   Error(u32)
 }
-impl HasInterface<LoggerMsg> for LoggerMsg  {}
-impl HasInterface<String> for LoggerMsg {}
-impl HasInterface<u32> for LoggerMsg {}
-impl<Unified> SpecificInterface<Unified> for LoggerMsg where 
- Unified: Eq + Case<LoggerMsg> + Case<String> + Case<u32> + Debug {
-  fn deserialize_as(item: Unified, bytes: Vec<u8>) ->
-   Result<Self, DeserializeError<Unified>> {
-    if <Unified as Case<LoggerMsg>>::VARIANT == item {
-      deserialize::<Unified, LoggerMsg, LoggerMsg>(item, bytes)
-    } else if <Unified as Case<u32>>::VARIANT == item {
-      deserialize::<Unified, LoggerMsg, u32>(item, bytes)
-    } else if <Unified as Case<String>>::VARIANT == item {
-      deserialize::<Unified, LoggerMsg, String>(item, bytes)
-    } else {
-      Result::Err(DeserializeError::IncompatibleInterface(item))
-    }
-  }
-}
 
-#[derive(Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
-enum DataStoreMsg { Get, Put(String) }
+#[derive(AurumInterface, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[aurum]
+enum DataStoreMsg { 
+  Get, 
+  Put(String) 
+}
 
 unified! { MsgTypes = LoggerMsg | DataStoreMsg | MaybeString | String | Unsigned32 | RegMsg}
 
 fn main() {
-  let r = Registry::<MsgTypes> {register: HashMap::new()};
+  forge_test();
+  serde_test();
+}
 
+fn serde_test() {
+  let ser_u32 = serde_json::to_string(&5u32).unwrap();
+  println!("u32 serialized: {}", ser_u32);
+
+  let ser_string = serde_json::to_string(&String::from("oh no!")).unwrap();
+  println!("string serialized: {}", ser_string);
+
+  let ser_lgr = serde_json::to_string(&LoggerMsg::Info(String::from("hello"))).unwrap();
+  println!("LoggerMsg::Info serialized: {}", ser_lgr);
+
+  let ser_get = serde_json::to_string(&DataStoreMsg::Get).unwrap();
+  println!("DataStoreMsg::Get serialized: {}", ser_get);
+
+  let ser_put = serde_json::to_string(&DataStoreMsg::Put(String::from("put"))).unwrap();
+  println!("DataStoreMsg::Put serialized: {}", ser_put);
+
+
+
+  let de_u32 = <LoggerMsg as SpecificInterface<MsgTypes>>
+    ::deserialize_as(<MsgTypes as Case<u32>>::VARIANT, ser_u32.into_bytes()).unwrap();
+  println!("u32 deserialized: {:?}", de_u32);
+
+  let de_string = <LoggerMsg as SpecificInterface<MsgTypes>>
+    ::deserialize_as(<MsgTypes as Case<String>>::VARIANT, ser_string.into_bytes()).unwrap();
+  println!("string deserialized: {:?}", de_string);
+
+  let de_lgr = <LoggerMsg as SpecificInterface<MsgTypes>>
+    ::deserialize_as(<MsgTypes as Case<LoggerMsg>>::VARIANT, ser_lgr.into_bytes()).unwrap();
+  println!("LoggerMsg::Info deserialized: {:?}", de_lgr);
+
+  let de_get = <DataStoreMsg as SpecificInterface<MsgTypes>>
+    ::deserialize_as(<MsgTypes as Case<DataStoreMsg>>::VARIANT, ser_get.into_bytes()).unwrap();
+  println!("DataStoreMsg::Get deserialized: {:?}", de_get);
+
+  let de_put = <DataStoreMsg as SpecificInterface<MsgTypes>>
+    ::deserialize_as(<MsgTypes as Case<DataStoreMsg>>::VARIANT, ser_put.into_bytes()).unwrap();
+  println!("DataStoreMsg::Put deserialized: {:?}", de_put);
+}
+
+fn forge_test() {
   let node = Node::new(Host::DNS("localhost".to_string()), 1000, 1001);
   let _lgr_msg = <MsgTypes as Case<LoggerMsg>>::forge
     ::<LoggerMsg>("logger".to_string(), node.clone());
   println!("logger ref: {:#?}", _lgr_msg);
+
   let _err_msg = <MsgTypes as Case<LoggerMsg>>::forge
     ::<u32>("logger".to_string(), node.clone());
-  println!("errors ref: {:#?}", _err_msg);
-  let w = "warning".to_string();
-  println!("translation warning \"{}\" to {:?}", w.clone(), LoggerMsg::from(w));
-  let x = 5u32;
-  println!("translation warning \"{}\" to {:?}", x.clone(), LoggerMsg::from(x));
+  println!("logger ref u32 interface: {:#?}", _err_msg);
+
+  let _warn_msg = <MsgTypes as Case<LoggerMsg>>::forge
+    ::<String>("logger".to_string(), node.clone());
+  println!("logger ref string interface: {:#?}", _warn_msg);
+
+  let _ds_msg = <MsgTypes as Case<DataStoreMsg>>::forge
+    ::<DataStoreMsg>("data_store".to_string(), node.clone());
+  println!("data store ref: {:#?}", _ds_msg);
 }
