e7fab5bf74cf78a4da842b1b9f0506700da9c390
Arjun Shukla <arjunlalshukla@gmail.com>
2021-03-03 11:26:25 -0700

    Added coverage.sh

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+ssh onyx "
+cd ~/research/thesis/aurum;
+git pull;
+cargo tarpaulin --exclude-files code-gen/* --exclude-files tests/* --exclude-files src/macros/* --out Xml;
+"
+scp onyx:~/research/thesis/aurum/cobertura.xml .
+pycobertura show --format html --output coverage.html cobertura.xml
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome coverage.html
+
