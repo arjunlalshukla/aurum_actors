f198588f2002d19e9ac836cdc1b7a11a29569036
Arjun Shukla <arjunlalshukla@gmail.com>
2021-03-08 21:42:21 -0700

    Fixed coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,7 @@
 ssh onyx "
 cd ~/research/thesis/aurum;
 git pull;
+cargo test --no-run;
 cargo tarpaulin --exclude-files code-gen/* --exclude-files tests/* --exclude-files src/macros/* --out Xml;
 "
 scp onyx:~/research/thesis/aurum/cobertura.xml .
